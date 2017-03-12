e0a0bc0d0e515e34bca1d0afe9b6c263b398e24f
Devendra Lattu <dev12@umbc.edu>
2017-03-12 18:25:55 -0400

    Create TwoSum.java

---
+++ Easy/TwoSum.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/two-sum/
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        
+        int index = 0;
+        for(int n : nums) {
+            if(null != map && map.containsKey(target - n)) {
+                int index_0 = map.get(target - n);
+                return new int[]{index_0, index};
+            }
+            map.put(n, index++);
+        }
+        
+        return new int[]{};
+    }
+}
