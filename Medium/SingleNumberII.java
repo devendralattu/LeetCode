4bb891bb2e1b65686d41fbf94a7119cf62531d23
Devendra Lattu <dev12@umbc.edu>
2017-04-01 00:16:57 -0400

    Create SingleNumberII.java

---
+++ Medium/SingleNumberII.java
@@ -0,0 +1,27 @@
+/* https://leetcode.com/problems/single-number-ii */
+// Given an array of integers, every element appears three times except for one, 
+// which appears exactly once. Find that single one.
+
+public int singleNumber(int[] nums) {
+        
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            
+            //iterating for every bit starting from LSB
+            int sum = 0;
+            for(int j = 0; j < nums.length; j++){
+                
+                //check if the 'i'th bit is set
+                if(((nums[j] >> i) & 1) == 1){
+                    sum++;
+                    sum %= 3;
+                }
+            }
+            
+            if(sum == 1){
+                ans |= (sum << i);
+            }
+        }
+        
+        return ans;
+}
