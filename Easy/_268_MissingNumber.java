79b1ffe3c5c06385db7cec9c7b5e2951a7f20d83
Devendra Lattu <dev12@umbc.edu>
2017-06-01 16:59:00 -0400

    Create _268_MissingNumber.java

---
+++ Easy/_268_MissingNumber.java
@@ -0,0 +1,11 @@
+// Given nums = [0, 1, 3] return 2. 
+
+public int missingNumber(int[] nums) {
+
+  int xor = 0, i = 0;
+  for (i = 0; i < nums.length; i++) {
+    xor = xor ^ i ^ nums[i];
+  }
+
+  return xor ^ i;
+}
