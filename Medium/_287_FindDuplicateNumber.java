e46ae1a26dfa525a6aa1ec85e245c07b8cb29927
Devendra Lattu <dev12@umbc.edu>
2017-06-02 19:27:48 -0400

    Update _287_FindDuplicateNumber.java

--- Medium/_287_FindDuplicateNumber.java
+++ Medium/_287_FindDuplicateNumber.java
@@ -4,6 +4,28 @@
     Assume that there is only one duplicate number, find the duplicate one. 
 */
 
+// WITHOUT modifying the array : similar to finding loop in Linked List
+public int findDuplicate(int[] nums) {
+    if(nums == null || nums.length == 0) return 0;
+
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+
+    while(slow != fast) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+
+    fast = 0;
+    while(slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
+
+// WITH array modification O(n)
 public int findDuplicate(int[] nums) {
     if(nums == null) return 0;
 
