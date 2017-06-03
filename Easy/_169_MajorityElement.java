69f47d8c706b7128792c53fa9c322b9a76a6eb4d
Devendra Lattu <dev12@umbc.edu>
2017-06-02 22:11:47 -0400

    Update _169_MajorityElement.java

--- Easy/_169_MajorityElement.java
+++ Easy/_169_MajorityElement.java
@@ -1,23 +1,36 @@
+
 /*
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-
 You may assume that the array is non-empty and the majority element always exist in the array.
 */
+
+// Good Reference : https://gregable.com/2013/10/majority-vote-algorithm-find-majority.html
+
 public int majorityElement(int[] nums) {
+
+    if(nums == null || nums.length == 0) return -1;
+
     int count = 0;
-    int major = 0;
+    int candidate = nums[0];
 
     for(int n : nums){
-        if(count == 0){
-            major = n;
-        }
-        if(major == n){
+        if(candidate == n) {
             count++;
-        }
-        else{
+        } else if(count == 0) {
+            candidate = n;
+            count++;
+        } else {
             count--;
         }
     }
 
-    return major;
+    // verification stage
+    count = 0;
+    for(int n : nums) {
+        if(n == candidate) {
+            count++;
+        }
+    }
+
+    return (count >= nums.length / 2) ? candidate : -1;
 }
