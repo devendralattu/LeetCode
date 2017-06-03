22f3c61d4a443413f15463664358270a07c7981e
Devendra Lattu <dev12@umbc.edu>
2017-06-02 20:21:05 -0400

    Update and rename ProductOfArrayExceptSelf.java to _238_ProductOfArrayExceptSelf.java

---
+++ Medium/_238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,20 @@
+/** https://leetcode.com/problems/product-of-array-except-self **/
+
+public int[] productExceptSelf(int[] nums) {
+    if(nums == null) return new int[]{};
+
+    int[] result = new int[nums.length];
+    result[0] = 1;
+
+    for(int i = 1; i < nums.length; i++) {
+        result[i] = result[i - 1] * nums[i - 1];
+    }
+
+    int product = 1;
+    for(int i = nums.length - 1; i >= 0; i--) {
+        result[i] *= product;
+        product *= nums[i];
+    }
+
+    return result;
+}
