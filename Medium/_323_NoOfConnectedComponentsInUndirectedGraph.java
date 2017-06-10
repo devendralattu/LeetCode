e5563aa056990809c229925d2e2368e8d858731c
Devendra Lattu <dev12@umbc.edu>
2017-06-10 12:52:42 -0400

    Create _323_NoOfConnectedComponentsInUndirectedGraph.java

---
+++ Medium/_323_NoOfConnectedComponentsInUndirectedGraph.java
@@ -0,0 +1,37 @@
+package medium;
+
+public class NumberOfConnectedCompUndirGraph {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NumberOfConnectedCompUndirGraph g = new NumberOfConnectedCompUndirGraph();
+		int[][] edges = new int[][] { { 0, 1 }, { 0, 2 }, { 1, 2 }, { 5, 1 }, { 3, 4 }, { 3, 6 } };
+		int result = g.countComponents(7, edges);
+		System.out.println(result);
+	}
+
+	public int countComponents(int n, int[][] edges) {
+		int[] roots = new int[n];
+		for (int i = 0; i < n; i++)
+			roots[i] = i;
+
+		for (int[] e : edges) {
+			int root1 = find(roots, e[0]);
+			int root2 = find(roots, e[1]);
+
+			if (root1 != root2) {
+				roots[root1] = root2; // union
+				n--;
+			}
+		}
+		return n;
+	}
+
+	public int find(int[] roots, int id) {
+		while (roots[id] != id) {
+			roots[id] = roots[roots[id]]; // optional: path compression
+			id = roots[id];
+		}
+		return id;
+	}
+}
