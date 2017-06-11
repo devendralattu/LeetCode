0672cb68b82cbca04cda2def8fc529548050094b
Devendra Lattu <dev12@umbc.edu>
2017-06-10 23:08:57 -0400

    Create MergeTwoBinaryTrees.java

---
+++ Contest/MergeTwoBinaryTrees.java
@@ -0,0 +1,12 @@
+public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+    if (t1 == null)
+        return t2;
+    if (t2 == null)
+        return t1;
+
+    TreeNode temp = new TreeNode(t1.val + t2.val);
+    temp.left = mergeTrees(t1.left, t2.left);
+    temp.right = mergeTrees(t1.right, t2.right);
+
+    return temp;
+}
