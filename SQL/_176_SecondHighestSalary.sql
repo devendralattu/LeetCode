dbfc888a5e1935e54f52145d75a96e66e22b1b8c
Devendra Lattu <dev12@umbc.edu>
2017-08-19 20:46:57 -0400

    Create _176_SecondHighestSalary.sql

---
+++ SQL/_176_SecondHighestSalary.sql
@@ -0,0 +1,20 @@
+# Write a SQL query to get the second highest salary from the Employee table.
+# return null if there is no second highest salary
+
+-- mtd 1
+SELECT MAX(e.salary) as SecondHighestSalary
+FROM employee e
+WHERE e.salary < (
+                    SELECT MAX(e2.salary) 
+                    FROM employee e2
+                );
+              
+              
+-- mtd 2   
+SELECT (
+        SELECT DISTINCT e.Salary
+        FROM Employee e
+        ORDER BY e.Salary DESC
+        LIMIT 1 OFFSET 1
+        ) 
+AS SecondHighestSalary;
