ef5e4405d300ccad43aa62be9a3027e3f6685079
Devendra Lattu <dev12@umbc.edu>
2017-08-20 02:28:26 -0400

    Update _176_SecondHighestSalary.sql

--- SQL/_176_SecondHighestSalary.sql
+++ SQL/_176_SecondHighestSalary.sql
@@ -15,6 +15,6 @@ SELECT (
         SELECT DISTINCT e.Salary
         FROM Employee e
         ORDER BY e.Salary DESC
-        LIMIT 1 OFFSET 1
+        LIMIT 1 OFFSET 1          -- LIMIT 1,1
         ) 
 AS SecondHighestSalary;
