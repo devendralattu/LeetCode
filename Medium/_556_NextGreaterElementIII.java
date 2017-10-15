d6cf0085c6237d7804fdb23e6e24b3c07c731c6b
Devendra Lattu <dev12@umbc.edu>
2017-10-15 13:44:28 -0400

    Update _556_NextGreaterElementIII.java

--- Medium/_556_NextGreaterElementIII.java
+++ Medium/_556_NextGreaterElementIII.java
@@ -1,13 +1,17 @@
 /*
-		Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has exactly the same digits existing in the integer n and is greater in value than n. If no such positive 32-bit integer exists, you need to return -1.
+	Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has exactly the same digits existing in the integer n and is greater in value than n. If no such positive 32-bit integer exists, you need to return -1.
 
-		Example 1:
-		Input: 12
-		Output: 21
+	Example 1:
+	Input: 12
+	Output: 21
 
-		Example 2:
-		Input: 21
-		Output: -1
+	Example 2:
+	Input: 21
+	Output: -1
+	
+	3465321 => 3512346
+	3464321 => 3612344
+
 */
 
 public int nextGreaterElement(int n) {
