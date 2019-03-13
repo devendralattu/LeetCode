7a8428d0b3b126d14a2bbc5d27c29c550d9e6bc1
Devendra Lattu <dev12@umbc.edu>
2019-03-12 22:56:02 -0400

    Update _20_ValidParentheses.java

--- Easy/_20_ValidParentheses.java
+++ Easy/_20_ValidParentheses.java
@@ -4,6 +4,8 @@
     The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 */
 
+/*
+//Old code
 public boolean isValid(String s) {
 
     if(s == null || s.length() % 2 == 1) {
@@ -26,4 +28,38 @@ public boolean isValid(String s) {
     }
 
     return stack.isEmpty();
+}
+*/
+
+public boolean isValid(String s) {
+    if(s == null || s.length() == 0) return true;
+    if(s.length() % 2 == 1) return false;
+    Stack<Character> stack = new Stack<>();
+
+    // boolean isValidString = validateString(s); // check for indexOf() on brackets
+    for(char c : s.toCharArray()) {
+        if(isOpenBrace(c)) {
+            stack.push(c);
+        } else {
+            // empty stack or invaild opening brace
+            if(!isValidClosing(stack, c)) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+    }
+
+    return stack.isEmpty();
+}
+
+private static boolean isOpenBrace(char c) {
+    return c == '(' || c == '{' || c == '[';
+}
+private static boolean isValidClosing(Stack<Character> stack, char c) {
+    if (stack.isEmpty()) {
+        return false;
+    }
+    char open = stack.peek();
+    return (open == '(' ? c == ')' : (open == '{' ? c == '}' : (open == '[' ? c == ']' : false)));
 }
