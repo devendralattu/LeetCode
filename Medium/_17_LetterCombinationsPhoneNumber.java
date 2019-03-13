7a3d923607bb79d21b48e1206ddd3d1c1da4f210
Devendra Lattu <dev12@umbc.edu>
2019-03-12 21:48:53 -0400

    Update _17_LetterCombinationsPhoneNumber.java

--- Medium/_17_LetterCombinationsPhoneNumber.java
+++ Medium/_17_LetterCombinationsPhoneNumber.java
@@ -45,3 +45,51 @@ class Solution {
 		}
 	}
 }
+
+
+/*
+class Solution {
+    public List<String> letterCombinations(String digits) {
+
+        List<String> list = new ArrayList<>();
+        if(digits == null || digits.length() == 0) {
+            return list;
+        }
+        char[] numbers = digits.toCharArray();
+
+        List<List<Character>> dialPad = new ArrayList<>();
+        fillDialPad(dialPad);
+
+        numberCombination(numbers, dialPad, list, 0, new StringBuilder());
+        return list;
+    }
+
+    private static void numberCombination(char[] numbers, List<List<Character>> dialPad, List<String> list,
+                                          int i, StringBuilder sb) {
+        if (i == numbers.length) {
+            list.add(sb.toString());
+        } else {
+            int num = Character.getNumericValue(numbers[i]);
+            for (int pos = 0; pos < dialPad.get(num).size(); pos++) {
+                sb.append(dialPad.get(num).get(pos));
+                numberCombination(numbers, dialPad, list, i + 1, sb);
+                // delete last char
+                sb.setLength(sb.length() - 1);
+            }
+        }
+    }
+
+    private static void fillDialPad(List<List<Character>> dialPad) {
+        dialPad.add(0, new ArrayList<>(Arrays.asList(' ')));
+        dialPad.add(1, new ArrayList<>(Arrays.asList('*')));
+        dialPad.add(2, new ArrayList<>(Arrays.asList('a', 'b', 'c')));
+        dialPad.add(3, new ArrayList<>(Arrays.asList('d', 'e', 'f')));
+        dialPad.add(4, new ArrayList<>(Arrays.asList('g', 'h', 'i')));
+        dialPad.add(5, new ArrayList<>(Arrays.asList('j', 'k', 'l')));
+        dialPad.add(6, new ArrayList<>(Arrays.asList('m', 'n', 'o')));
+        dialPad.add(7, new ArrayList<>(Arrays.asList('p', 'q', 'r', 's')));
+        dialPad.add(8, new ArrayList<>(Arrays.asList('t', 'u', 'v')));
+        dialPad.add(9, new ArrayList<>(Arrays.asList('w', 'x', 'y', 'z')));
+    }
+}
+*/
